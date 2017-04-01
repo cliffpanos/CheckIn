c76d6416f93c9c95ced4288cd1c8cb408c8f9dfc
cliffpanos <cliffpanos@gmail.com>
2017-04-01 01:24:06 -0400

    Changed file structure

---
+++ CheckIn iOS App/CheckIn/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  CheckIn
+//
+//  Created by Cliff Panos on 4/1/17.
+//  Copyright © 2017 Clifford Panos. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
