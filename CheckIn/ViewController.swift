67ab411711433f7c7f6ea4091ba1540f89737e80
cliffpanos <cliffpanos@gmail.com>
2017-04-01 01:16:24 -0400

    Initial commit for iOS App

---
+++ CheckIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckIn
+//
+//  Created by Cliff Panos on 4/1/17.
+//  Copyright © 2017 Clifford Panos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
